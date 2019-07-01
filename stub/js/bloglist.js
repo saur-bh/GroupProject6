f9a61c443532adc5221818d7b6c5c837d5139086
ShirinRadhanpura <shirin.radhanpura@gmail.com>
2019-07-01 12:56:06 +0530

    Part B of the solutuion

---
+++ stub/js/bloglist.js
@@ -0,0 +1,12 @@
+function funDisplay(){
+    document.getElementById('myModal3').style.display = 'block';
+}
+
+function hideDisplay(){
+    document.getElementById('myModal3').style.display = 'none';
+}
+   
+function nextPage(){
+   window.open('post.html');
+}
+
